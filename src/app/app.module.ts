24af2fbf28a85555f3d1b98b2c2c5450d658f8f4
TakesxiSximada <8707279+TakesxiSximada@users.noreply.github.com>
2017-12-18 10:45:26 +0900

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroesService } from './hero.service';
 import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
+import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
@@ -15,10 +19,12 @@ import { MessageService } from './message.service';
         HeroesComponent,
         HeroDetailComponent,
         MessagesComponent,
+        DashboardComponent,
     ],
     imports: [
         BrowserModule,
         FormsModule,
+        AppRoutingModule,
     ],
     providers: [MessageService],
     bootstrap: [AppComponent],
