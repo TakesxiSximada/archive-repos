c7f9764feba1d49a3b5408ab33efcc569a23300e
TakesxiSximada <8707279+TakesxiSximada@users.noreply.github.com>
2019-07-12 17:03:10 +0900

    Add square_add_loop function

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,13 @@ fn square_loop(mut x: f64) {
     }
 }
 
+fn square_add_loop (c: f64) {
+    let mut x = 0;
+    loop {
+        x = x * x + c;
+    }
+}
+
 
 fn main() {
     println!("Hello, world!");
