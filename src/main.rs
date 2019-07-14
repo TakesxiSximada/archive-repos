a479dee59e7dcd06432224eff9b0f16eab255d39
TakesxiSximada <8707279+TakesxiSximada@users.noreply.github.com>
2019-07-14 12:09:33 +0900

    Add mandelbrot code and command line argument test

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,64 @@
+#[allow(dead_code)]
+
+extern crate num;
+
+use std::str::FromStr;
+use num::Complex;
+
+
+fn parse_pair<T: FromStr> (s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => {
+            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+                (Ok(l), Ok(r)) => Some((l, r)),
+                _ => None
+            }
+        }
+    }
+}
+
+
+#[test]
+fn test_parse_pair() {
+    assert_eq!(parse_pair::<i32>("", ','), None);
+    assert_eq!(parse_pair::<i32>("10,", ','), None);
+    assert_eq!(parse_pair::<i32>(",10", ','), None);
+    assert_eq!(parse_pair::<i32>("10,20", ','), Some((10, 20)));
+    assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
+    assert_eq!(parse_pair::<f64>("0.5x", 'x'), None);
+    assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
+}
+
+
+fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    for i in 0..limit {
+        z = z * z + c;
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+    }
+    None
+}
+
+fn complex_square_and_loop (c: Complex<f64>) {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    loop {
+        z = z * z + c;
+    }
+}
+
+
 fn square_loop(mut x: f64) {
     loop {
         x = x * x;
     }
 }
 
+
 fn square_add_loop (c: f64) {
-    let mut x = 0;
+    let mut x: f64 = 0.0;
     loop {
         x = x * x + c;
     }
