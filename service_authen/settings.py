77042f8f7ee4aef6614f0a48ab65e59f972c60f0
TakesxiSximada <8707279+TakesxiSximada@users.noreply.github.com>
2019-07-23 19:03:07 +0900

    Add local SMTP server settings

--- service_authen/settings.py
+++ service_authen/settings.py
@@ -86,3 +86,11 @@ ADMINS = (
     ('sximada', 'sximada@example.com'),
 )
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = '127.0.0.1'
+EMAIL_PORT = 1025
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
+
+
